8a7d78bcf0a75bc22c51da17da96b141d8162632
studentuser <studentuser@studentusers-macbook-pro.local>
2015-10-08 17:40:10 -0700

    Initial Commit

---
+++ calculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  calculator
+//
+//  Created by studentuser on 10/8/15.
+//  Copyright © 2015 kartikey. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
